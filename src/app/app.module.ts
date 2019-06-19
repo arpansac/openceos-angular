6c1fcc2c8504ce0e2916f8a5d55461557173788f
arpansac <arpansac@gmail.com>
2019-06-20 02:27:43 +0530

    create a header and a footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+import { AppHomeComponent } from './components/app-home/app-home.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { MainNavComponent } from './components/shared/main-nav/main-nav.component';
+import { AngularMaterialModule } from './shared-modules/angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHomeComponent,
+    FooterComponent,
+    MainNavComponent,
   ],
   imports: [
+    AngularMaterialModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared-modules/angular-material.module.ts
+++ src/app/shared-modules/angular-material.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatMenuModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+
   ],
   exports: [
-    
+    MatMenuModule,
+    MatTabsModule,
+    MatToolbarModule,
   ]
 })
 export class AngularMaterialModule { }
